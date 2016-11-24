7c31c0ed13068f53f76c5aa3f1623e79f118019c
Teacher <001@sapa-astana.kz>
2016-11-24 14:27:16 +0600

    this is my second commit

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -5,7 +5,7 @@ public class ConsoleApp {
 
     public static void main(String[] args){
 
-        System.out.println("=================");
+//        System.out.println("=================");
         System.out.println("Hello World\nPlease enter some symbol:");
         System.currentTimeMillis();
         try {
