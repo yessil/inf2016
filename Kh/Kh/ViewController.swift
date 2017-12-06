1f1a7bbffbc5daa48283d131d31b214f774a6c1b
Aziz Kalbayev <kalbayev_01@Azizs-iMac.local>
2017-12-06 20:45:50 +0800

    ASD

--- Kh/Kh/ViewController.swift
+++ Kh/Kh/ViewController.swift
@@ -1,25 +1,42 @@
-//
-//  ViewController.swift
-//  Kh
-//
-//  Created by Guest User on 11/9/17.
-//  Copyright © 2017 Guest User. All rights reserved.
-//
-
 import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create destination URL
+        let documentsUrl:URL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first as URL!
+        let destinationFileUrl = documentsUrl.appendingPathComponent("downloadedFile.mp3")
+        
+        //Create URL to the source file you want to download
+        let fileURL = URL(string: "https://www.emp3dl.co/download/WrsFXgQk5UI/lil-uzi-vert-xo-tour-llif3-music.html")
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let request = URLRequest(url:fileURL!)
+        
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // Success
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    print("Successfully downloaded. Status code: \(statusCode)")
+                }
+                
+                do {
+                    try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                } catch (let writeError) {
+                    print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                }
+                
+            } else {
+                print("Error took place while downloading a file. Error description: %@", error?.localizedDescription);
+            }
+        }
+        task.resume()
+        
     }
-
-
 }
 
